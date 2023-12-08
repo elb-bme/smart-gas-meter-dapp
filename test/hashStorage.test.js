c7cd98a559e6d32843d29f9c2159f08519cc7c9f
elb-bme <79838492+elb-bme@users.noreply.github.com>
2023-12-08 03:24:04 +0100

    Environment configuration, project setup, dependencies

---
+++ test/hashStorage.test.js
@@ -0,0 +1,6 @@
+/* Framework: Mocha and Chai.
+Tests:
+Test hash storage and retrieval functionalities.
+Test access control mechanisms.
+Reasoning: Unit tests validate individual components of the contract in isolation,
+ ensuring that each function behaves as expected. */
