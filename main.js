67f5372bc6c34023d6955013fcc270dd19db1281
elb-bme <79838492+elb-bme@users.noreply.github.com>
2023-12-10 13:41:32 +0100

    data reader improved

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const fastcsv = require('fast-csv');
-const Web3 = require('web3');
+const { v4: uuidv4 } = require('uuid');
+const { Web3 } = require('web3');
+
 require('dotenv').config();
 
 // Configure Web3 instance for Volta Testnet
-const web3 = new Web3(new Web3.providers.HttpProvider('https://volta-rpc.energyweb.org'));
+const web3 = new Web3('https://volta-rpc.energyweb.org');
 
 // Mock smart contract interaction function
 const sendHashToContract = async (hash) => {
@@ -18,18 +20,28 @@ const sendHashToContract = async (hash) => {
 
 // Function to read meter data from CSV
 function readMeterData(meterId, callback) {
-    let data = [];
-    fs.createReadStream(path.join(__dirname, 'meter_data.csv'))
-      .pipe(fastcsv.parse({ headers: true }))
-      .on('data', (row) => {
-        if (row.id === meterId) {
-          data.push(row);
-        }
-      })
-      .on('end', () => {
-        callback(data);
-      });
-  }
+    console.log(`Reading meter data for ID: ${meterId}`);
+    let found = false; // Variable to indicate if the matching record was found
+    fs.createReadStream(path.join(__dirname, 'data', 'meter_data.csv'))
+        .pipe(fastcsv.parse({ headers: true }))
+        .on('data', (row) => {
+            if (row["Meter ID"].trim() === meterId) { // Trim to remove whitespace
+                found = true; // Set found to true when found the record
+                callback(row);
+                this.destroy(); // Stop reading the CSV file
+            }
+        })
+        .on('end', () => {
+            if (!found) {
+                console.log(`No data found for Meter ID: ${meterId}`);
+                callback(null); // Call the callback with null to indicate no data found
+            }
+        })
+        .on('error', (error) => {
+            console.error('Error reading CSV file:', error.message);
+            callback(null); // Call the callback with null due to the error
+        });
+}
 
 // Function to generate a mock DID
 const generateDID = () => {
@@ -55,8 +67,17 @@ const createAndStoreVC = (data, did, hash) => {
             hash: hash
         }
     };
-    fs.writeFileSync(`vcs/${did}.csv`, JSON.stringify(vc) + '\n');
-}
+
+    try {
+        if (!fs.existsSync('vcs')){
+            fs.mkdirSync('vcs');
+        }
+        fs.writeFileSync(`vcs/${did}.json`, JSON.stringify(vc, null, 2));
+        console.log(`VC for DID: ${did} written to file.`);
+    } catch (error) {
+        console.error('Error writing VC to file:', error);
+    }
+};
 
 // Main function to process meter data
 const processMeterData = (meterId) => {
@@ -69,8 +90,10 @@ const processMeterData = (meterId) => {
                 createAndStoreVC(record, did, hash);
                 sendHashToContract(hash);
             });
+            rl.close(); // Close the readline interface after processing
         } else {
             console.log('No data found for the given meter ID.');
+            rl.close(); // Close the readline interface if no data found
         }
     });
 }
@@ -84,7 +107,6 @@ const rl = readline.createInterface({
   // Prompt user for Meter ID
   rl.question('Enter the Meter ID: ', (meterId) => {
     processMeterData(meterId);
-    rl.close();
   });
   
   rl.on('close', () => {
